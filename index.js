d565a0a3ce8c62fa92105c22165318a366763102
Yurina Shin <yurinas@gmail.com>
2020-06-10 04:07:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function createPost(){
+  let commenter = document.getElementById(`commenterName`).value;
+  let comment = document.getElementById(`commentText`).value;
+  let commentTemplate =
+  `<div class="comment><p><%= comment %></p>,p>Posted By:<span class"commenter"><%=commenter %></span></p></div>`;
+
+}
